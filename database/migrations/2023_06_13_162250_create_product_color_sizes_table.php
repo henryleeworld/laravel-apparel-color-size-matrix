e9435cf7596a3257e80a42cbdb19792013966792
Henry Lee <henryleeworld@gmail.com>
2024-04-13 00:06:47 +0800

    feat:升級至 laravel 11

--- database/migrations/2023_06_13_162250_create_product_color_sizes_table.php
+++ database/migrations/2023_06_13_162250_create_product_color_sizes_table.php
@@ -4,8 +4,12 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
-    public function up()
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
     {
         Schema::create('product_color_sizes', function (Blueprint $table) {
             $table->foreignId('product_id')->constrained()->onDelete('cascade');
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // User::factory(10)->create();
+
         User::factory()->create([
-            'name' => 'Test User',
+            'name' => __('Administrator'),
             'email' => 'admin@admin.com',
         ]);
 
